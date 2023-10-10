ceb0ba77d6197f74abcff3211e770fda54c9e8bd
Mohameddeya18 <144900366+Mohameddeya18@users.noreply.github.com>
2023-10-10 15:29:23 +0400

    Exercise 1

---
+++ Chapter 1- Getting Started with Python/Exercises/sec try.py
@@ -0,0 +1,20 @@
+f_name="mohammed"
+l_name="hamad"
+name=f_name+l_name
+print(name,end="/")
+print("welcome to code lab 1")
+
+name="bibo"
+print(f'the name is {name}.')
+
+
+import turtle
+turtle.circle(120)
+turtle.forward(100)
+turtle.circle(90)
+turtle.backward(350)
+turtle.circle(90)
+turtle.forward(60)
+turtle.circle(150)
+turtle.forward(70)
+turtle.circle(200)
