135153118eae53b94ed59d1203b72d06cc466907
Mohameddeya18 <144900366+Mohameddeya18@users.noreply.github.com>
2023-10-25 12:00:42 +0400

    my repository

---
+++ Chapter 4- Control Flow/Exercises/chapter4_exercise5.py
@@ -0,0 +1,18 @@
+#my favourite fruits
+
+favorite_fruits = ['apples', 'bananas', 'dragon fruit' , 'orange',]
+
+if 'bananas' in favorite_fruits:
+    print("I  like the bannas most!")
+
+if 'apples' in favorite_fruits:
+    print("I like apples!")
+
+if 'dragon fruit' in favorite_fruits:
+    print("I like dragon fruit!")
+
+if 'oranges' in favorite_fruits:
+    print("I do like orange but not my favourite!")
+
+if 'strawberries' in favorite_fruits:
+    print("I hate strawberries!")
